c6e5c7324efd06a5bd737585a6c32e7e31a26087
Steven Kamwaza <48084787+StevenKamwaza@users.noreply.github.com>
2023-03-22 16:44:54 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String btnName = "click";
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +21,53 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter 1 App"),
+          title: const Text("Flutter Intro"),
+        ),
+        body: Center(
+          //child: const Text("Body"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    btnName = "Loading...";
+                  });
+                },
+                child: Text(btnName),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    btnName = "Loading...";
+                  });
+                },
+                child: Text(btnName),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_outlined),
+              label: "Dashboard",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: "Settings",
+            )
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
       ),
     );
